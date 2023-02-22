4b0b1b5033c2f97f0ed0f2756011e4dbe2530fdc
Satyam Pund <pundsatyam@gmail.com>
2023-02-22 21:21:27 +0530

    feat: user model validations

--- server/models/User.js
+++ server/models/User.js
@@ -2,10 +2,24 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
   {
-    fullName: String,
-    phone: String,
-    email: String,
-    password: String,
+    fullName: {
+      type: String,
+      required: [true, 'Please enter your Name'],
+    },
+    phone: {
+      type: String,
+      required: [true, 'Please enter your Phone'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please enter your Email'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please enter your Password'],
+      minlength: [6, 'Password must include more than 6 characters'],
+    },
   },
   { timestamps: true }
 );
