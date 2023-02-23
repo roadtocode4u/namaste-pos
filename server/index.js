442cbd52ed11a480ca9f0a918c5023984cbad082
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-02-23 14:01:44 +0530

    feat:create sign up api

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+import User from './models/User.js'
+dotenv.config();
+mongoose.set('strictQuery', false);
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+
+try {
+  mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log('Connected to DB 📦');
+  });
+} catch (err) {
+  console.log(`❌ Error:  ${err?.message}`);
+}
+
 
 app.get('/health', (req, res) => {
   res.json({
@@ -11,6 +25,35 @@ app.get('/health', (req, res) => {
     message: 'Server is running'
   })
 })
+
+app.post('/signup',async(req,res )=>{
+  const { fullName, phone, email, password } = req.body;
+
+  const emptyFields = [];
+
+  if (emptyFields.length > 0) {
+    return res.json({
+      success: false,
+      message: `${emptyFields.join(", ")} are required`,
+    });
+  }
+
+    const user = new User({
+      fullName: fullName,
+      phone: phone,
+      email: email,
+      password: password,
+    });
+  
+    const savedUser = await user.save();
+  
+    res.send({
+      success: true,
+      message: "Signup successfully...",
+      data: savedUser,
+    });
+});
+
 
 app.listen(PORT, () => {
   console.log(`The server is running on port ${PORT} 🚀`);
