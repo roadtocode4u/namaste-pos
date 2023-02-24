68014195dd8f53c0e925396ed1d1d53553b9d601
Vaibhavihole31 <itspinki05@gmail.com>
2023-02-24 11:30:29 +0530

    feat: home view and navbar component created

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './views/Home/Home'
 import Signup from './views/Signup/Signup'
 import Login from './views/Login/Login'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/login' element={<Login />} />
         </Routes>
