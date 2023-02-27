ad6b158678cef3769abec2672a03b52e800bb112
sakshi-rah <rahangdalesakshi2@gmail.com>
2023-02-27 15:20:32 +0530

    feat: product-category-validation

--- server/models/Product-Category.js
+++ server/models/Product-Category.js
@@ -1,14 +1,31 @@
 import mongoose, { Schema } from "mongoose";
 
-const productCategorySchema = mongoose.Schema({
-    categoryType: String,
-    categoryTitle: String,
-    isCategoryAvailable: String,
-    catUpdateTime: Number,
-    itemImgUrl: String
-}, {
-    timestamps: true
-});
+const productCategorySchema = mongoose.Schema(
+    {
+
+        categoryType: {
+            type: String,
+            required: true
+        },
+
+        categoryTitle: {
+            type: String,
+            required: true
+        },
+
+        isCategoryAvailable: Boolean,
+
+        catUpdateTime: Date,
+
+        itemImgUrl: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
 
 const ProductCategory = mongoose.model("ProductCategory", productCategorySchema);
 
