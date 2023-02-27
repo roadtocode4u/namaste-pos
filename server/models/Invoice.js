0e913ba09929ef574d008d05ac56eafe1accf697
Vaibhavihole31 <itspinki05@gmail.com>
2023-02-27 20:36:15 +0530

    feat: validation for invoice model

--- server/models/Invoice.js
+++ server/models/Invoice.js
@@ -1,11 +1,34 @@
 import mongoose from "mongoose";
 
 const invoiceSchema = new mongoose.Schema({
-    invoiceNumber: Number,
-    invoiceDate: String,
-    invoiceTotal: Number,
-    discount: Number,
-    tax: String
+    invoiceNumber: {
+        type: Number,
+        required: [true, "invoiceNumber annot be empty"]
+    },
+    invoiceDate: {
+        type: String,
+        required: [true, "invoiceDate cannot be empty"]
+    },
+    invoiceTotal: {
+        type: Number,
+        required: [true, "invoiceTotal cannot be empty"]
+    },
+    discount: {
+        type: Number,
+        required: [true, "discount cannot be empty"]
+    },
+    tax: {
+        type: String,
+        requied: [true, "tax cannot be empty"]
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    order: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Order"
+    }
 },
     {
         timestamps: true
