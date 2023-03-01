b3565f968d242634d8da9261253148922c233835
Suraj Shende <surajshende247@gmail.com>
2023-03-01 09:46:03 +0530

    feat: REST APIs for book added

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import User from './models/User.js';
+import Book from './models/Book.js';
 dotenv.config();
 mongoose.set('strictQuery', false);
 
@@ -13,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 try {
   mongoose.connect(
-    process.env.MONGODB_URL,
+    'mongodb://localhost:27017/namaste-pos',
     { useNewUrlParser: true, useUnifiedTopology: true },
     () => {
       console.log('Connected to DB 📦');
@@ -117,6 +118,108 @@ app.post('/login', async (req, res) => {
     });
   }
 });
+
+/*
+ Sample API Routes Starts Here
+*/
+
+// POST book  => create book
+app.post('/book', async (req, res) => {
+  const { title, author, price } = req.body;
+  // validations will go here
+  const book = new Book({
+    title,
+    author,
+    price,
+  });
+
+  const savedBook = await book.save();
+
+  res.json({
+    success: true,
+    message: 'Book created successfully',
+    data: savedBook,
+  });
+});
+
+// GET book/:id => get book by id
+app.get('/book/:id', async (req, res) => {
+  const { id } = req.params;
+  const book = await Book.findById(id);
+
+  res.json({
+    success: true,
+    message: 'Book fetched successfully',
+    data: book,
+  });
+});
+
+// GET book?title= => get book by title
+app.get('/book', async (req, res) => {
+  const { title } = req.query;
+  const book = await Book.findOne({ title });
+
+  res.json({
+    success: true,
+    message: 'Book fetched successfully',
+    data: book,
+  });
+});
+
+// GET books => get all books
+app.get('/books', async (req, res) => {
+  const books = await Book.find();
+
+  res.json({
+    success: true,
+    message: 'Books fetched successfully',
+    data: books,
+  });
+});
+
+// PUT book/:id => update book by id
+app.put('/book/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title, author, price } = req.body;
+
+  await Book.updateOne(
+    {
+      _id: id,
+    },
+    {
+      $set: {
+        title,
+        author,
+        price,
+      },
+    }
+  );
+
+  const updatedBook = await Book.findById(id);
+
+  res.json({
+    success: true,
+    message: 'Book updated successfully',
+    data: updatedBook,
+  });
+});
+
+// DELETE book/:id => delete book by id
+app.delete('/book/:id', async (req, res) => {
+  const { id } = req.params;
+  const book = await Book.deleteOne({
+    _id: id,
+  });
+
+  res.json({
+    success: true,
+    message: 'Book deleted successfully',
+    data: book,
+  });
+});
+/*
+ Sample API Routes Ends Here
+*/
 
 app.listen(PORT, () => {
   console.log(`The server is Running on Port ${PORT} 🚀`);
