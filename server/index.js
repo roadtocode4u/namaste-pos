c0e5760b5ca9ddd18a48782c1efb6dfc886e4c91
Suraj Shende <surajshende247@gmail.com>
2023-03-04 09:52:08 +0530

    ref: move auth routes to controllers

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 import express from 'express';
-import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import User from './models/User.js';
 
 dotenv.config();
 mongoose.set('strictQuery', false);
@@ -16,6 +14,10 @@ import {
   putBook,
   deleteBook,
 } from './controllers/book.js';
+
+import { postSignup, postLogin } from './controllers/auth.js';
+
+import { getHealth } from './controllers/general.js';
 
 const app = express();
 app.use(express.json());
@@ -34,100 +36,16 @@ try {
   console.log(`❌ Error:  ${err?.message}`);
 }
 
-app.get('/health', (req, res) => {
-  res.json({
-    status: 'ok',
-    message: 'Server is running',
-  });
-});
+app.get('/health', getHealth);
 
-app.post('/signup', async (req, res) => {
-  const { fullName, phone, email, password } = req.body;
-
-  const emptyFields = [];
-
-  if (!fullName) emptyFields.push('fullName');
-  if (!phone) emptyFields.push('phone');
-  if (!email) emptyFields.push('email');
-  if (!password) emptyFields.push('password');
-
-  if (emptyFields.length > 0) {
-    return res.json({
-      success: false,
-      message: `${emptyFields.join(', ')} is required`,
-    });
-  }
-
-  // validations to check if email already exist start
-  const existingUser = await User.findOne({ email: email });
-  if (existingUser) {
-    return res.json({
-      success: false,
-      message: 'Email already exists',
-    });
-  }
-
-  // validations to check if phone already exist start
-  const existingUserPhone = await User.findOne({ phone: phone });
-  if (existingUserPhone) {
-    return res.json({
-      success: false,
-      message: 'Phone already exists',
-    });
-  }
-
-  try {
-    const user = new User({
-      fullName,
-      phone,
-      email,
-      password,
-    });
-
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
-
-    const savedUser = await user.save();
-
-    res.json({
-      success: true,
-      message: 'Signup successfully...',
-      data: savedUser,
-    });
-  } catch (err) {
-    res.json({
-      success: false,
-      message: err.message,
-    });
-  }
-});
-
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.json({
-      success: false,
-      message: 'Email and password are required',
-    });
-  }
-
-  const user = await User.findOne({ email });
-  const validPassword = await bcrypt.compare(password, user.password);
-
-  if (validPassword) {
-    return res.json({
-      success: true,
-      message: 'User logged in successfully',
-      user: user,
-    });
-  } else {
-    return res.json({
-      success: false,
-      message: 'Username or Password is incorrect',
-    });
-  }
-});
+/*
+  Auth API Routes Starts Here
+*/
+app.post('/signup', postSignup);
+app.post('/login', postLogin);
+/*
+  Auth API Routes Ends Here
+*/
 
 /*
  Book API Routes Starts Here
