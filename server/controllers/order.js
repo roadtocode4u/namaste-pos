8dd86d9d8db5d5e825ba98c63e856b48f79a89ba
Satyam Pund <pundsatyam@gmail.com>
2023-03-04 22:41:37 +0530

    ref: move postOrder function to controllers

---
+++ server/controllers/order.js
@@ -0,0 +1,44 @@
+import Order from "./../models/Order.js";
+
+/*----- 1-create order API -----*/
+export const postOrder =  async (req, res) => {
+    const { userId, tableNumber, orderType, items, orderComments } = req.body;
+  
+    const totalOrders = await Order.countDocuments();
+    const orderId = totalOrders + 1;
+  
+    // validations to check if all the required fields are filled or not
+    const requiredFields = ['tableNumber', 'items', 'orderType'];
+    const emptyFields = requiredFields.filter((field) => !req.body[field]);
+  
+    if (emptyFields.length > 0) {
+      return res.json({
+        success: false,
+        message: `${emptyFields.join(', ')} cannot be empty`,
+      });
+    }
+  
+    try {
+      const order = new Order({
+        orderId,
+        userId,
+        tableNumber,
+        orderType,
+        items,
+        orderComments,
+      });
+  
+      const savedOrder = await order.save();
+  
+      res.json({
+        success: true,
+        message: 'Order placed successfully',
+        data: savedOrder,
+      });
+    } catch (err) {
+      res.json({
+        success: false,
+        message: err.message,
+      });
+    }
+  }
--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ import {
   putInvoice,
   deleteInvoice,
 } from './controllers/invoice.js';
+import { postOrder } from './controllers/order.js';
 
 const app = express();
 app.use(express.json());
@@ -236,47 +237,7 @@ app.delete('/productItem/:id', async (req, res) => {
 
 /*----- 1-create order API -----*/
 
-app.post('/order', async (req, res) => {
-  const { userId, tableNumber, orderType, items, orderComments } = req.body;
-
-  const totalOrders = await Order.countDocuments();
-  const orderId = totalOrders + 1;
-
-  // validations to check if all the required fields are filled or not
-  const requiredFields = ['tableNumber', 'items', 'orderType'];
-  const emptyFields = requiredFields.filter((field) => !req.body[field]);
-
-  if (emptyFields.length > 0) {
-    return res.json({
-      success: false,
-      message: `${emptyFields.join(', ')} cannot be empty`,
-    });
-  }
-
-  try {
-    const order = new Order({
-      orderId,
-      userId,
-      tableNumber,
-      orderType,
-      items,
-      orderComments,
-    });
-
-    const savedOrder = await order.save();
-
-    res.json({
-      success: true,
-      message: 'Order placed successfully',
-      data: savedOrder,
-    });
-  } catch (err) {
-    res.json({
-      success: false,
-      message: err.message,
-    });
-  }
-});
+app.post('/order', postOrder);
 
 /*----- 2-Get orders API -----*/
 
