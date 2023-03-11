4bd28cd83862d49a35064d5a79dfe032181c620e
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-03-11 19:31:34 +0530

    ref:solve conflicts

--- server/index.js
+++ server/index.js
@@ -2,17 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-import User from './models/User.js';
-import ProductCategory from './models/ProductCategory.js';
-import DiningTable from './models/DiningTable.js';
-import { postProductCategory ,getProductCategoryTitle,getProductCategories,putProductCategoryId ,deleteProductCategoryId}
- from './controllers/productCategory.js'
-import{postProductItem, getProductItemById, getProductItemTitle, getProductItems, putProductItem, deleteProductItem} from './controllers/productItem.js'
-
-
 dotenv.config();
 mongoose.set('strictQuery', false);
-
 
 import { getHealth } from './controllers/health.js';
 
