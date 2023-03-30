1964c62ef45cb2c66e82d50468457c599b229558
vaishnavihole <vaishnavihole1@gmail.com>
2023-03-30 11:51:22 +0530

    feat: create add table basic form

---
+++ client/src/admin/views/AddTable/AddTable.js
@@ -0,0 +1,125 @@
+import React, { useState } from 'react';
+import './AddTable.css';
+import axios from 'axios';
+import swal from 'sweetalert';
+//import marketplaceImg from './marketplace-img.png';
+
+
+function AddTable() {
+  const [tableNumber, setTableNumber] = useState('');
+  const [capacity, setCapacity] = useState('');
+  const [tableLocation, setTableLocation] = useState('');
+  const [tableService, setTableService] = useState('');
+  
+
+  async function addTable() {
+    if (!tableNumber || !capacity || !tableLocation || !tableService) {
+      await swal({
+        title: 'Please fill all the fields',
+        text: response.data.message,
+        icon: 'error',
+        button: '😥',
+      });
+      return;
+    }
+    const response = await axios.post('/createDiningTable', {
+        tableNumber,
+        capacity,
+        tableLocation,
+        tableService,
+    });
+
+    if (response.data.success) {
+      await swal({
+        title: 'Table Added Successfully!!',
+        text: response.data.message,
+        icon: 'success',
+        button: 'Aww yiss!',
+      });
+    } else {
+      swal(response.data.message);
+    }
+
+
+
+    setTableNumber('');
+    setCapacity('');
+    setTableLocation('');
+    setTableService('');
+  }
+
+  return (
+    <>
+      <div className="container">
+        <h4 className="text-center heading">Add Table </h4>
+        <div className="main-card-div text-center">
+          <div className="row">
+            <div className="col-md-5 mx-auto d-block">
+              {/* <img src={marketplaceImg} className="add-product-img" /> */}
+            </div>
+            <div className="col-md-7">
+              <form>
+                <div className="mb-4 mt-2">
+                  <input
+                    type="text"
+                    className="add-table-form-input"
+                    id="tableNumber"
+                    placeholder="Table Number"
+                    value={tableNumber}
+                    onChange={(e) => {
+                      setTableNumber(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="text"
+                    className="add-table-form-input"
+                    id="capacity"
+                    placeholder="Capacity"
+                    value={capacity}
+                    onChange={(e) => {
+                      setCapacity(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="text"
+                    className="add-table-form-input"
+                    id="tableLocation"
+                    placeholder="Table Location"
+                    value={tableLocation}
+                    onChange={(e) => {
+                      setTableLocation(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="text"
+                    className="add-table-form-input"
+                    id="tableService"
+                    placeholder="Table Service"
+                    value={tableService}
+                    onChange={(e) => {
+                      setTableService(e.target.value);
+                    }}
+                  />
+                </div>
+                <button
+                  className="button-add-material w-100 mb-4"
+                  type="button"
+                  onClick={addTable}>
+                  <i className="fa-solid fa-right-to-bracket"></i>{' '}
+                  <b>Add Table</b>
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default AddTable;
