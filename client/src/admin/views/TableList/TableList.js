8620245eeb95ea28461635bc110c99c751807630
Vaibhavihole31 <itspinki05@gmail.com>
2023-04-02 00:39:43 +0530

    feat: added qr code in table list

--- client/src/admin/views/TableList/TableList.js
+++ client/src/admin/views/TableList/TableList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './TableList.css'
 import axios from 'axios';
+import QRCode from 'qrcode';
 
 function TableList() {
     const [table, setTable] = useState([]);
@@ -14,6 +15,35 @@ function TableList() {
     useEffect(() => {
         fetchTalbles();
     }, []);
+
+    const generateQRCode = async (tableNumber) => {
+        const link = `http://localhost:3000/bookTable/${tableNumber}`;
+        try {
+            console.log(await QRCode.toDataURL(link));
+            // 1. Generate the QR code data URL
+            const qrCodeDataURL = await QRCode.toDataURL(link);
+
+            // 2. Create a new anchor element to hold the download link
+            const downloadLink = document.createElement('a');
+
+            // 3. Set the href attribute of the anchor element to the QR code data URL
+            downloadLink.href = qrCodeDataURL;
+
+            // 4. Set the download attribute of the anchor element to the desired file name
+            downloadLink.download = `table_${tableNumber}_QRCode.png`;
+
+            // 5. Add the anchor element to the DOM (hidden)
+            document.body.appendChild(downloadLink);
+
+            // 6. Programmatically trigger the click event on the anchor element
+            downloadLink.click();
+
+            // 7. Remove the anchor element from the DOM
+            document.body.removeChild(downloadLink);
+        } catch (err) {
+            console.error(err);
+        }
+    };
     return (
         <>
             <div style={{ marginTop: '70px' }}>
@@ -26,7 +56,7 @@ function TableList() {
                             <th>Location</th>
                             <th style={{ width: '250px' }}>User</th>
                             <th style={{ width: '250px' }}>Phone</th>
-                            {/* <th style={{ width: '150px' }}>QR Code</th> */}
+                            <th style={{ width: '150px' }}>QR Code</th>
                             <th style={{ width: '150px' }}>Actions</th>
                         </tr>
                     </thead>
@@ -39,14 +69,21 @@ function TableList() {
                                     <td>{item.tableService}</td>
                                     <td>{item.tableLocation}</td>
                                     <td>
-                                    {item.occupiedBy?.fullName}
+                                        {item.occupiedBy?.fullName}
                                     </td>
                                     <td>
-                                    {item.occupiedBy?.phone}
+                                        {item.occupiedBy?.phone}
                                     </td>
 
 
-                                    {/* <td><button></button></td> */}
+                                    <td>
+                                        <button
+                                            className="text-center qr-code-btn"
+                                            onClick={() => {
+                                                generateQRCode(item.tableNumber);
+                                            }}>
+                                            <b>QR Code</b>
+                                        </button></td>
                                     <td>
                                         <button className="mx-3 list-update-btn">
                                             <b>Update</b>
