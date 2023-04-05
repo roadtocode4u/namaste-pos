f8e5c04e70b60b3706969fb699b5bf971d3cbb43
Satyam Pund <pundsatyam@gmail.com>
2023-04-05 13:30:06 +0530

    feat: login required to book table by QR code

--- client/src/views/BookTable/BookTableLogic.js
+++ client/src/views/BookTable/BookTableLogic.js
@@ -3,28 +3,32 @@ import axios from 'axios';
 import swal from 'sweetalert';
 
 import { currentUser } from '../../utils/auth';
+import { loginRequired } from '../../utils/loginRequired';
 
 const BookTableLogic = () => {
   const alreadyCalled = useRef(false);
   const tableBooking = async () => {
     const url = window.location.pathname;
+    localStorage.setItem('tableBooking', JSON.stringify(url));
     const tableNumber = url.split('/').pop();
     const userId = currentUser._id;
-    console.log(tableNumber);
     const response = await axios.post(`/bookTable/${tableNumber}`, {
       userId: userId,
     });
-    console.log(response.data.message);
-    console.log(typeof response.data.data.occupied);
 
     await swal({
       text: response.data.message,
       button: 'Okay',
     });
     window.location.href = '/';
+
+    if (response.data.data.occupied) {
+      localStorage.removeItem('tableBooking');
+    }
   };
 
   useEffect(() => {
+    loginRequired();
     if (!alreadyCalled.current) {
       alreadyCalled.current = true;
       tableBooking();
