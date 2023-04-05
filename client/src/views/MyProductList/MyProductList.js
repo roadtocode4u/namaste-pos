efe3fccd2ec3bfe9ebe1aab8a1fd38a798542cbd
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-04-05 14:52:44 +0530

    change the utton and add shoping card icon in navbar

--- client/src/views/MyProductList/MyProductList.js
+++ client/src/views/MyProductList/MyProductList.js
@@ -6,32 +6,29 @@ function MyProductList() {
   return (
     <>
       <div>
-        <h1 className="text-center heading mb-5">🍽 MyList</h1>
+        <h1 className="text-center my-product-list-heading">🍽 MyList</h1>
         {myProductListItems.map((item, index) => {
           return (
-            <div key={index} className='container'>
-         
-            <div key={index} className="product-card">
-              <h4>{item.name}</h4>
-              <b>Quantity: {item.quantity}</b>{' '}
-              <b className="mb-2 product-card-price">{item.price}</b>
-              <img className="product-img" src={item.imgUrl} />
-              <div className='row'>
-              <div className='col-md-6'>
-                <button className="remove-btn"><b>Remove</b></button>
+            <div key={index} className="container">
+              <div key={index} className="product-card">
+                <h4>{item.name}</h4>
+                <b>Quantity: {item.quantity}</b>{' '}
+                <b className="mb-2 product-card-price">{item.price}</b>
+                <img className="product-img" src={item.imgUrl} />
+                <div className="text-center">
+                  <button className="btn btn-danger remove-btn">
+                    <b>Remove</b>
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className='col-md-6'>
-                <button className="add-btn"><b>Add</b></button>
-            </div>
-            </div>
-            </div>
-            </div>
-            
-           
           );
         })}
         <div className="text-center">
-          <button className="btn btn-success confirm-btn">
+          <button className="btn btn-success Addmore-btn">
+            <b>Add More Items</b>
+          </button>
+          <button className="btn btn-warning confirm-btn">
             <b>Confirm Orders</b>
           </button>
         </div>
