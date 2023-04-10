40f96ee93511166ef823198a89ae233b2a8482a4
Vaibhavihole31 <itspinki05@gmail.com>
2023-04-10 13:53:06 +0530

    ref: home image added

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,45 +1,39 @@
 import React from 'react';
-
 import { Link } from 'react-router-dom';
 
 import './Home.css';
-
 import { pendingBooking } from '../../utils/auth';
+import HomePage from './background-img.jpg'
 
 function Home() {
   return (
     <>
-      <div className="main-div">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="text-center">
-              <div className="text-center namstePOS-container">
-                <p className="namastePOS-heading">NamastePOS</p>
+      <div className='main-div'>
+        <div className='container-fluid'>
+          <div className='row'>
+          <h3 className='text-center namastePOS-heading'>NamastePOS</h3>
+            <div className='col-md-6'>
+              <div className='text-center'>
+                <img className='home-page-img mb-5' src={HomePage} />
               </div>
-              <div className='row'>
-                <div className='col-md-6'>
-                  <h5 className="homepage-heading">
-                    WORK TOGETHER, <br />
-                    EAT TOGETHER
-                  </h5>
-                </div>
-                <div className='col-md-6'>
-                 <div className='order-btn-container d-flex justify-content-evenly'>
-                 <Link to="/menu">
-                    <button className="order-btn">Menu</button>
-                  </Link>
+            </div>
+            <div className='col-md-6 item'>
+              <h4 className='text-center homepage-heading'>  WORK TOGETHER, <br />
+                EAT TOGETHER</h4>
+              <div className='order-btn-container d-flex justify-content-evenly'>
+                <Link to="/menu">
+                  <button className="order-btn">Menu</button>
+                </Link>
 
-                  {pendingBooking ? (
-                    <Link to={`${pendingBooking}`}>
-                      <button className="order-btn">Book Table</button>
-                    </Link>
-                  ) : (
-                    <Link to="/bookTable">
-                      <button className="order-btn">Book Table</button>
-                    </Link>
-                  )}
-                 </div>
-                </div>
+                {pendingBooking ? (
+                  <Link to={`${pendingBooking}`}>
+                    <button className="order-btn">Book Table</button>
+                  </Link>
+                ) : (
+                  <Link to="/bookTable">
+                    <button className="order-btn">Book Table</button>
+                  </Link>
+                )}
               </div>
             </div>
           </div>
